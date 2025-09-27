f82548caa860fbb51cbf1922dd31f70c9d416c08
Dima_V <astrallux777@ukr.net>
2025-09-27 23:23:42 +0300

    27.09.2025_N3-01

--- src/models/note.js
+++ src/models/note.js
@@ -1,7 +1,51 @@
 // src/models/note.js
 
-import { Schema } from 'mongoose';
-import { model } from 'mongoose';
+// import { Schema } from 'mongoose';
+// import { model } from 'mongoose';
+
+// const noteSchema = new Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: true,
+//     },
+//     content: {
+//       type: String,
+//       default: '',
+//     },
+//     tag: {
+//       type: String,
+//       enum: [
+//         'Work',
+//         'Personal',
+//         'Meeting',
+//         'Shopping',
+//         'Ideas',
+//         'Travel',
+//         'Finance',
+//         'Health',
+//         'Important',
+//         'Todo',
+//       ],
+//       default: 'Todo',
+//     },
+//   },
+//   {
+//     timestamps: true,
+//     versionKey: false,
+//   },
+// );
+
+// export const Note = model('Note', noteSchema);
+
+
+
+
+
+// src/models/note.js
+
+import { Schema, model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -15,18 +59,7 @@ const noteSchema = new Schema(
     },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS, // використовуємо константи з tags.js
       default: 'Todo',
     },
   },
